8c89325bf4965a612061ade19fda0db8ef373301
Pamela Karina Canale <pamela.karina@hotmail.com>
2025-08-20 21:52:16 -0300

    Desafio 2 - Modelando sistema de celulares

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", "32");   
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
+Console.WriteLine("------------------------------");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("987654321", "iPhone 13", "987654321098765", "128");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Safari");
+Console.WriteLine("------------------------------");
